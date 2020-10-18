67784a30c77014d3c47930d760ab385f445569ee
ofridman <olivergf@gmail.com>
2020-10-18 18:58:12 +0200

    First commit

---
+++ Unity FSM State Machine C#/Assets/Unity State Machine C#/Example/Scripts/EnemyAI.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AxlPlay
+{
+    public class EnemyAI : MonoBehaviour
+    {
+        public enum States
+        {
+            Wander,
+            Pursue,
+        }
+
+        public StateMachine<States> fsm;
+
+        private Wander _wander;
+        private Pursue _pursue;
+        private FieldOfView _fieldOfView;
+
+        void Awake()
+        {
+            _wander = GetComponent<Wander>();
+            _pursue = GetComponent<Pursue>();
+            _fieldOfView = GetComponent<FieldOfView>();
+
+            //Initialize State Machine Engine		
+            fsm = StateMachine<States>.Initialize(this);
+        }
+        void Start()
+        {
+
+            fsm.ChangeState(States.Wander);
+
+        }
+        private void Update()
+        {
+
+            _fieldOfView.FindVisibleTargets();
+
+        }
+        void Wander_Enter()
+        {
+            _wander.fsm.ChangeState(Wander.States.Wander);
+        }
+        void Wander_Update()
+        {
+            if (_fieldOfView.visibleTargets.Count > 0)
+                fsm.ChangeState(States.Pursue);
+        }
+        void Wander_Exit()
+        {
+            _wander.fsm.ChangeState(Wander.States.Finish);
+
+        }
+
+        void Pursue_Enter()
+        {
+            
+            _pursue.fsm.ChangeState(Pursue.States.Pursue);
+        }
+        void Pursue_Update()
+        {
+            if (_fieldOfView.visibleTargets.Count == 0)
+                fsm.ChangeState(States.Wander);
+
+        }
+        void Pursue_Exit()
+        {
+            _pursue.fsm.ChangeState(Pursue.States.Idle);
+
+        }
+    }
+}
---
+++ Unity FSM State Machine C#/Assets/Unity State Machine C#/Example/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AxlPlay2
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float RotationSpeed = 5f;
+
+        public float Speed = 5f;
+
+        private Vector3 moveInput;
+        private Vector3 forward;
+        private Vector3 right;
+
+        private CharacterController characterController;
+
+        private void Awake()
+        {
+            characterController = GetComponent<CharacterController>();
+        }
+
+    
+        void Update()
+        {
+        
+
+                GetInputAndValues();
+                Move();
+                Rotate();
+
+        }
+        void GetInputAndValues()
+        {
+            moveInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+            //get the input direction for the camera position.
+
+
+
+            forward = Camera.main.transform.TransformDirection(Vector3.forward);
+            forward.y = 0f;
+            forward = forward.normalized;
+            right = new Vector3(forward.z, 0.0f, -forward.x);
+        }
+        void Move()
+        {
+            var _move = (moveInput.x * right + moveInput.y * forward);
+
+            _move.Normalize();
+
+
+            if (_move != Vector3.zero)
+            {
+                characterController.SimpleMove(_move * Speed);
+
+
+            }
+        }
+        void Rotate()
+        {
+            var _direction = (moveInput.x * right + moveInput.y * forward);
+            if (_direction != Vector3.zero)
+            {
+
+                transform.forward = Vector3.Lerp(transform.forward, _direction, Time.deltaTime * RotationSpeed);
+            }
+        }
+
+    }
+}
